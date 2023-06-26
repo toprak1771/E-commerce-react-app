200279ceaad4e484642a29e11ddf058a6eb1734e
Toprak <94640883+toprak1771@users.noreply.github.com>
2023-06-26 14:15:25 +0300

    complete order operations (#3)
    Co-authored-by: Toprak Tüzün <topraktuzun17@hotmail.com>

---
+++ src/api/OrderApi.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import axios from 'axios';
+
+export const OrderApi = async (input) => {
+  const token = localStorage.getItem('access_token');
+  console.log("token:",token);
+  const { data } = await axios.post(`http://localhost:4000/order`, input, {
+    headers: {
+      authorization: token,
+    },
+  });
+  return data;
+};
